84fb92728077dcd599f640fe06b8ad5ae608213d
= <peeranat85@gmail.com>
2015-10-11 23:38:52 +0700

    apply angular-ui for confirmDelete modal

--- webapp/src/main/webapp/bpproperty/app/property/land-installment-controller.js
+++ webapp/src/main/webapp/bpproperty/app/property/land-installment-controller.js
@@ -4,19 +4,69 @@
 
   angular
 
-    .module('land-installment', ['ngRoute', 'my-notification', 'land-installment-service'])
+    .module('land-installment', ['ngRoute', 'ui.bootstrap', 'my-notification', 'land-installment-service'])
 
-  .controller('InstallmentListCtrl', ['$scope', '$route', 'LandBuyService', 'InstallmentService', 'NotificationService',
-    function($scope, $route, LandBuyService, InstallmentService, NotificationService) {
+  .controller('ConfirmDeleteModalCtrl', ['$rootScope', '$scope', '$route', '$modalInstance',
+    'InstallmentService', 'NotificationService', 'installment',
+    function($rootScope, $scope, $route, $modalInstance, InstallmentService, NotificationService, installment) {
 
-      var self = this;
+      $scope.delete = function() {
+        var criteria = {
+          landId: $route.current.params['landId'],
+          buyDetailId: $route.current.params['buyDetailId'],
+          installmentId: $scope.installment.id
+        };
+
+        InstallmentService.delete(criteria).then(function(response) {
+            NotificationService.notify({
+              type: 'success',
+              msg: 'Installment deleted'
+            });
+
+            // emit to load installments
+            $rootScope.$broadcast('loadInstallments');
+          },
+          function(error) {
+            NotificationService.notify({
+              type: 'error',
+              msg: 'Unable to delete the selected Installment'
+            });
+          });
+
+        $modalInstance.dismiss('cancel');
+      };
+
+      $scope.closeModal = function() {
+        $modalInstance.dismiss('cancel');
+      };
+
+      $scope.installment = installment;
+    }
+  ])
+
+  .controller('InstallmentListCtrl', ['$scope', '$route', '$uibModal', 'LandBuyService', 'InstallmentService', 'NotificationService',
+    function($scope, $route, $uibModal, LandBuyService, InstallmentService, NotificationService) {
+
+      this.confirmDeleteModal = function(selected) {
+
+        $scope.modalInstance = $uibModal.open({
+          animation: true,
+          templateUrl: 'myModalContent.html',
+          controller: 'ConfirmDeleteModalCtrl',
+          resolve: {
+            installment: function() {
+              return selected;
+            }
+          }
+        });
+      };
 
       var installmentDialog = $('#installmentDialog').on('hidden.bs.modal', function(e) {
         $scope.installment = {};
       });
 
-      var confirmDeleteDialog = $('#confirmDeleteDialog').on('hidden.bs.modal', function(e) {
-        $scope.installment = {};
+      $scope.$on('loadInstallments', function() {
+        self.loadInstallments();
       });
 
       $scope.saveInstallment = function(isValid) {
@@ -81,34 +131,7 @@
         installmentDialog.modal();
       };
 
-      $scope.confirmDeleteDialog = function(installment) {
-        $scope.installment = installment;
-        confirmDeleteDialog.modal();
-      }
-
-      $scope.deleteInstallment = function() {
-        var deleteCriteria = {
-          landId: $route.current.params['landId'],
-          buyDetailId: $route.current.params['buyDetailId'],
-          installmentId: $scope.installment.id
-        };
-        InstallmentService.delete(deleteCriteria).then(function(response) {
-            NotificationService.notify({
-              type: 'success',
-              msg: 'Installment deleted'
-            });
-            self.loadInstallments();
-          },
-          function(error) {
-            NotificationService.notify({
-              type: 'error',
-              msg: 'Unable to delete existing Installment'
-            });
-          });
-
-        confirmDeleteDialog.modal('hide'); // hide dialog
-      }
-
+      var self = this;
       var installmentCriteria = {
         landId: $route.current.params['landId'],
         buyDetailId: $route.current.params['buyDetailId'],
@@ -120,7 +143,6 @@
       };
 
       this.loadInstallments = function() {
-
         InstallmentService.query(installmentCriteria).then(function(data) {
           $scope.installments = data;
           return LandBuyService.query(landBuyCriteria);
@@ -132,56 +154,52 @@
       };
 
       var currentTime = new Date();
-
       var currentYear = currentTime.getFullYear();
       var currentMonth = currentTime.getMonth();
 
-      var months = [{
+      $scope.months = [{
         key: 0,
-        value: 'มกราคม'
+        value: '\u0e21\u0e01\u0e23\u0e32\u0e04\u0e21' // January
       }, {
         key: 1,
-        value: 'กุมภาพันธ์'
+        value: '\u0e01\u0e38\u0e21\u0e20\u0e32\u0e1e\u0e31\u0e19\u0e18\u0e4c' // February
       }, {
         key: 2,
-        value: 'มีนาคม'
+        value: '\u0e21\u0e35\u0e19\u0e32\u0e04\u0e21' // March
       }, {
         key: 3,
-        value: 'เมษายน'
+        value: '\u0e40\u0e21\u0e29\u0e32\u0e22\u0e19' // April
       }, {
         key: 4,
-        value: 'พฤษภาคม'
+        value: '\u0e1e\u0e24\u0e29\u0e20\u0e32\u0e04\u0e21' // May
       }, {
         key: 5,
-        value: 'มิถุนายน'
+        value: '\u0e21\u0e34\u0e16\u0e38\u0e19\u0e32\u0e22\u0e19' // June
       }, {
         key: 6,
-        value: 'กรกฎาคม'
+        value: '\u0e01\u0e23\u0e01\u0e0e\u0e32\u0e04\u0e21' // July
       }, {
         key: 7,
-        value: 'สิงหาคม'
+        value: '\u0e2a\u0e34\u0e07\u0e2b\u0e32\u0e04\u0e21' // August
       }, {
         key: 8,
-        value: 'กันยายน'
+        value: '\u0e01\u0e31\u0e19\u0e22\u0e32\u0e22\u0e19' // September
       }, {
         key: 9,
-        value: 'ตุลาคม'
+        value: '\u0e15\u0e38\u0e25\u0e32\u0e04\u0e21' // October
       }, {
         key: 10,
-        value: 'พฤศจิกายน'
+        value: '\u0e1e\u0e24\u0e28\u0e08\u0e34\u0e01\u0e32\u0e22\u0e19' // November
       }, {
         key: 11,
-        value: 'ธันวาคม'
+        value: '\u0e18\u0e31\u0e19\u0e27\u0e32\u0e04\u0e21' // December
       }];
-
-      var years = [currentYear - 3, currentYear - 2, currentYear - 1, currentYear];
 
       this.loadInstallments();
 
       $scope.installment = {};
-      $scope.months = months;
-      $scope.years = years;
-      $scope.selectedMonth = months[currentMonth];
+      $scope.years = [currentYear - 3, currentYear - 2, currentYear - 1, currentYear];
+      $scope.selectedMonth = $scope.months[currentMonth];
       $scope.selectedYear = currentYear;
 
     }
